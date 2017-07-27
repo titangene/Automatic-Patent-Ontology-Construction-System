3e72e68de58322e2b0016d3114f4812dd28bf6a1
titangene <titangene01@gmail.com>
2017-07-28 00:43:17 +0800

    Add Create Ontology Data Property, Refactor code

--- src/OntologyConstructor/Execute_OntologyConstructor.java
+++ src/OntologyConstructor/Execute_OntologyConstructor.java
@@ -3,6 +3,6 @@ package OntologyConstructor;
 public class Execute_OntologyConstructor {
 	public static void main(String[] args) {
 		OntologyConstruction oc = new OntologyConstruction();
-		oc.getOntology();
+		oc.CreateOntology();
 	}
 }
--- src/OntologyConstructor/OntologyConstruction.java
+++ src/OntologyConstructor/OntologyConstruction.java
@@ -5,42 +5,84 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
+import edu.stanford.smi.protegex.owl.model.OWLDatatypeProperty;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
 import edu.stanford.smi.protegex.owl.writer.rdfxml.rdfwriter.OWLModelWriter;
 
 public class OntologyConstruction {
 	jdbcmysql mysql = new jdbcmysql();
-	public void getOntology() {
+	
+	OWLModel owlModel;
+	
+	OWLNamedClass patent_information_OWLClass;	// Class：專利資訊
+	OWLNamedClass patent_category_OWLClass;		// Class：專利分類
+	OWLNamedClass IPC_OWLClass;					// Class：國際分類號_IPC
+	OWLNamedClass LOC_OWLClass;					// Class：設計分類號_LOC
+//	OWLNamedClass patent_relationship_person_OWLClass;	// Class：專利關係人
+//	OWLNamedClass inventor_OWLClass;			// Class：發明人
+//	OWLNamedClass applicant_OWLClass;			// Class：申請人
+	
+	OWLDatatypeProperty patent_name_OWLDataProperty;
+	OWLDatatypeProperty application_date_OWLDataProperty;
+	OWLDatatypeProperty reference_OWLDataProperty;
+	OWLDatatypeProperty inventor_OWLDataProperty;
+	OWLDatatypeProperty applicant_OWLDataProperty;
+	
+	public void CreateOntology() {
 		try {
-			OWLModel owlModel = ProtegeOWL.createJenaOWLModel();
+			owlModel = ProtegeOWL.createJenaOWLModel();
+			CreateOWLClass(owlModel);
+			CreateOWLDataProperty(owlModel);
+			
 			ContentAnalysis(owlModel);
 			
-			FileOutputStream fileOutputStream = new FileOutputStream(new File("patent_ontology.owl"));
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
-			OWLModelWriter owlModelWriter = new OWLModelWriter(owlModel, owlModel.getTripleStoreModel().getActiveTripleStore(), outputStreamWriter);
-			owlModelWriter.write();
-			fileOutputStream.flush();
-			outputStreamWriter.flush();
-			fileOutputStream.close();
-			outputStreamWriter.close();
+			SaveOntology();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	void ContentAnalysis(OWLModel owlModel) throws Exception {
+	private void CreateOWLClass(OWLModel owlModel) throws Exception {
 		// 建立 "專利資訊" Class
-		OWLNamedClass patent_information_OWLClass = owlModel.createOWLNamedClass("專利資訊");
+		patent_information_OWLClass = owlModel.createOWLNamedClass("專利資訊");
 		// 在 "專利分類" class 中新增兩個 child class "國際分類號_IPC"、"設計分類號_LOC"
-		OWLNamedClass patent_category_OWLClass = owlModel.createOWLNamedClass("專利分類");
-		OWLNamedClass IPC_OWLClass = owlModel.createOWLNamedSubclass("國際分類號_IPC", patent_category_OWLClass);	
-		OWLNamedClass LOC_OWLClass = owlModel.createOWLNamedSubclass("設計分類號_LOC", patent_category_OWLClass);
+		patent_category_OWLClass = owlModel.createOWLNamedClass("專利分類");
+		IPC_OWLClass = owlModel.createOWLNamedSubclass("國際分類號_IPC", patent_category_OWLClass);	
+		LOC_OWLClass = owlModel.createOWLNamedSubclass("設計分類號_LOC", patent_category_OWLClass);
 		// 在 "專利關係人" class 中新增兩個 child class "發明人"、"申請人"
-		OWLNamedClass patent_relationship_person_OWLClass = owlModel.createOWLNamedClass("專利關係人");
-		OWLNamedClass inventor_OWLClass = owlModel.createOWLNamedSubclass("發明人", patent_relationship_person_OWLClass);
-		OWLNamedClass applicant_OWLClass = owlModel.createOWLNamedSubclass("申請人", patent_relationship_person_OWLClass);
+//		patent_relationship_person_OWLClass = owlModel.createOWLNamedClass("專利關係人");
+//		inventor_OWLClass = owlModel.createOWLNamedSubclass("發明人", patent_relationship_person_OWLClass);
+//		applicant_OWLClass = owlModel.createOWLNamedSubclass("申請人", patent_relationship_person_OWLClass);
+	}
+	
+	private void CreateOWLDataProperty(OWLModel owlModel) throws Exception {
+		patent_name_OWLDataProperty = owlModel.createOWLDatatypeProperty("專利名稱");
+		application_date_OWLDataProperty = owlModel.createOWLDatatypeProperty("申請日");
+		reference_OWLDataProperty = owlModel.createOWLDatatypeProperty("參考文獻");
+		inventor_OWLDataProperty = owlModel.createOWLDatatypeProperty("發明人");
+		applicant_OWLDataProperty = owlModel.createOWLDatatypeProperty("申請人");
+	}
+	
+	private void CreateOWLIndividual(OWLModel owlModel) throws Exception {
 		
+	}
+	
+	
+	private void ContentAnalysis(OWLModel owlModel) throws Exception {
+		
+	}
+	
+	private void SaveOntology() throws Exception {
+		FileOutputStream fileOutputStream = new FileOutputStream(new File("patent_ontology.owl"));
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
+		OWLModelWriter owlModelWriter = 
+				new OWLModelWriter(owlModel, owlModel.getTripleStoreModel().getActiveTripleStore(), outputStreamWriter);
+		owlModelWriter.write();
+		fileOutputStream.flush();
+		outputStreamWriter.flush();
+		fileOutputStream.close();
+		outputStreamWriter.close();
 		System.out.println("Create Ontology Successfully");
 	}
 }
