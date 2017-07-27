669c05330e76fd18c7207e383e70ab7d657681f7
titangene <titangene01@gmail.com>
2017-07-27 23:22:55 +0800

    Add Execute_OntologyConstructor

---
+++ src/OntologyConstructor/Execute_OntologyConstructor.java
@@ -0,0 +1,8 @@
+package OntologyConstructor;
+
+public class Execute_OntologyConstructor {
+	public static void main(String[] args) {
+		OntologyConstruction oc = new OntologyConstruction();
+		oc.getOntology();
+	}
+}
