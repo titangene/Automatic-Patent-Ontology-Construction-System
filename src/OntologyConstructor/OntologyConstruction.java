6b3e0b80d381c0c3b02a9b65f6dcad846bb0d2ad
titangene <titangene01@gmail.com>
2017-08-02 02:07:16 +0800

    Add Create OWL*, Delete more OWL*, Refactor code
    # Class
    ## Add
    - patentType
    - applicant_Personal
    - applicant_Company
    - applicant_School_OWLClass
    - country
        - country_Asia
        - country_Europe
        - country_Africa
        - country_NorthAmerica
        - country_SouthAmerica
        - country_Oceania
        - country_Other
    ## Delete
    - patentInformation
    
    # DataProperty
    ## Delete
    - inventor
    - applicant
    
    # Object Property
    ## Add
    - patentType
    
    # Individual
    ## Add
    - patentType_Invention
    - patentType_Model
    - patentType_Design

--- src/OntologyConstructor/OntologyConstruction.java
+++ src/OntologyConstructor/OntologyConstruction.java
@@ -25,28 +25,43 @@ public class OntologyConstruction {
 	OWLModel owlModel;
 	
 	// Class (類別)
-	OWLNamedClass patentInformation_OWLClass;	// 專利資訊
 	OWLNamedClass patentID_OWLClass;			// 專利編號
 	OWLNamedClass patentCategory_OWLClass;		// 專利分類
-	OWLNamedClass IPC_OWLClass;					// 國際分類號_IPC
-	OWLNamedClass LOC_OWLClass;					// 設計分類號_LOC
-//	OWLNamedClass patent_relationship_person_OWLClass;	// 專利關係人
-//	OWLNamedClass inventor_OWLClass;			// 發明人
-//	OWLNamedClass applicant_OWLClass;			// 申請人
+	OWLNamedClass patentCategory_IPC_OWLClass;	// 國際分類號_IPC
+	OWLNamedClass patentCategory_LOC_OWLClass;	// 設計分類號_LOC
+	OWLNamedClass patentType_OWLClass;			// 專利類型
+	OWLNamedClass patentRelationshipPerson_OWLClass;	// 專利關係人
+	OWLNamedClass inventor_OWLClass;			// 發明人
+	OWLNamedClass applicant_OWLClass;			// 申請人
+	OWLNamedClass applicant_Personal_OWLClass;	// 個人
+	OWLNamedClass applicant_Company_OWLClass;	// 企業
+	OWLNamedClass applicant_School_OWLClass;	// 學校
+	OWLNamedClass country_OWLClass;				// 國家
+	OWLNamedClass country_Asia_OWLClass;		// 亞洲
+	OWLNamedClass country_Europe_OWLClass;		// 歐洲
+	OWLNamedClass country_Africa_OWLClass;		// 非洲
+	OWLNamedClass country_NorthAmerica_OWLClass;// 北美洲
+	OWLNamedClass country_SouthAmerica_OWLClass;// 南美洲
+	OWLNamedClass country_Oceania_OWLClass;		// 大洋洲
+	OWLNamedClass country_Other_OWLClass;		// 其他
 	
 	// DataProperty (資料屬性)
-	OWLDatatypeProperty patentName_OWLDataProperty;		// 專利名稱
+	OWLDatatypeProperty patentName_OWLDataProperty;			// 專利名稱
 	OWLDatatypeProperty applicationDate_OWLDataProperty;	// 申請日
 	OWLDatatypeProperty reference_OWLDataProperty;			// 參考文獻
-	OWLDatatypeProperty inventor_OWLDataProperty;			// 發明人
-	OWLDatatypeProperty applicant_OWLDataProperty;			// 申請人
+	//OWLDatatypeProperty inventor_OWLDataProperty;			// 發明人
+	//OWLDatatypeProperty applicant_OWLDataProperty;		// 申請人
 	
 	// ObjectProperty (物件屬性)
 	OWLObjectProperty isReferencedBy_OWLObjectProperty;	// is_referenced_by (被參考)
+	OWLObjectProperty patentType_OWLObjectProperty;		// 專利類型為
 	
 	// Individual (實例)
 	OWLIndividual patentID_OWLIndividual;		// 專利編號
 	OWLIndividual patentID_IsReferencedBy_OWLIndividual;	// 專利編號 (被參考)
+	OWLIndividual patentType_Invention_OWLIndividual;		// 發明 (Invention)
+	OWLIndividual patentType_Model_OWLIndividual;			// 新型 (Model)
+	OWLIndividual patentType_Design_OWLIndividual;			// 新式樣/設計 (Design)
 	
 	String patentID;
 	String patentName;
@@ -67,6 +82,7 @@ public class OntologyConstruction {
 			owlModel = ProtegeOWL.createJenaOWLModel();
 			CreateOWLClass();
 			CreateOWLDataProperty();
+			CreateOWLIndividual();
 			CreateOWLObjectProperty();
 			
 			ContentAnalysis();
@@ -79,24 +95,52 @@ public class OntologyConstruction {
 	
 	/**
 	 * Class:
-	 * - 專利資訊
-	 * 	 - 專利編號
+	 * - 專利編號
+	 * - 專利類型
 	 * - 專利分類
 	 * 	 - 國際分類號_IPC
 	 * 	 - 設計分類號_LOC
+	 * - 專利關係人
+	 *   - 發明人
+	 *   - 申請人
+	 *     - 個人
+	 *     - 企業
+	 *     - 學校
+	 * - 國家
+	 *   - 亞洲
+	 *   - 歐洲
+	 *   - 非洲
+	 *   - 北美洲
+	 *   - 南美洲
+	 *   - 大洋洲
+	 *   - 其他
 	 */
 	private void CreateOWLClass() throws Exception {
-		// 建立 "專利資訊" Class 中新增一個 child class "專利編號"
-		patentInformation_OWLClass = owlModel.createOWLNamedClass("專利資訊");
-		patentID_OWLClass = owlModel.createOWLNamedSubclass("專利編號", patentInformation_OWLClass);	
+		// 建立 "專利資訊" Class
+		patentID_OWLClass = owlModel.createOWLNamedClass("專利編號");	
 		// 在 "專利分類" class 中新增兩個 child class "國際分類號_IPC"、"設計分類號_LOC"
 		patentCategory_OWLClass = owlModel.createOWLNamedClass("專利分類");
-		IPC_OWLClass = owlModel.createOWLNamedSubclass("國際分類號_IPC", patentCategory_OWLClass);	
-		LOC_OWLClass = owlModel.createOWLNamedSubclass("設計分類號_LOC", patentCategory_OWLClass);
+		patentCategory_IPC_OWLClass = owlModel.createOWLNamedSubclass("國際分類號_IPC", patentCategory_OWLClass);	
+		patentCategory_LOC_OWLClass = owlModel.createOWLNamedSubclass("設計分類號_LOC", patentCategory_OWLClass);
+		// 建立 "專利類型" Class
+		patentType_OWLClass = owlModel.createOWLNamedClass("專利類型");
+		// 在 "國家" class 中新增多個 child class "亞洲"、"歐洲"、"非洲"、"北美洲"、"南美洲"、"大洋洲"、"其他"
+		country_OWLClass = owlModel.createOWLNamedClass("國家");
+		country_Asia_OWLClass = owlModel.createOWLNamedSubclass("亞洲", country_OWLClass);
+		country_Europe_OWLClass = owlModel.createOWLNamedSubclass("歐洲", country_OWLClass);
+		country_Africa_OWLClass = owlModel.createOWLNamedSubclass("非洲", country_OWLClass);
+		country_NorthAmerica_OWLClass = owlModel.createOWLNamedSubclass("北美洲", country_OWLClass);
+		country_SouthAmerica_OWLClass = owlModel.createOWLNamedSubclass("南美洲", country_OWLClass);
+		country_Oceania_OWLClass = owlModel.createOWLNamedSubclass("大洋洲", country_OWLClass);
+		country_Other_OWLClass = owlModel.createOWLNamedSubclass("其他", country_OWLClass);
 		// 在 "專利關係人" class 中新增兩個 child class "發明人"、"申請人"
-//		patent_relationship_person_OWLClass = owlModel.createOWLNamedClass("專利關係人");
-//		inventor_OWLClass = owlModel.createOWLNamedSubclass("發明人", patent_relationship_person_OWLClass);
-//		applicant_OWLClass = owlModel.createOWLNamedSubclass("申請人", patent_relationship_person_OWLClass);
+		patentRelationshipPerson_OWLClass = owlModel.createOWLNamedClass("專利關係人");
+		inventor_OWLClass = owlModel.createOWLNamedSubclass("發明人", patentRelationshipPerson_OWLClass);
+		// 在 "申請人" class 中新增三個 child class "個人"、"企業"、"學校"
+		applicant_OWLClass = owlModel.createOWLNamedSubclass("申請人", patentRelationshipPerson_OWLClass);
+		applicant_Personal_OWLClass = owlModel.createOWLNamedSubclass("個人", applicant_OWLClass);
+		applicant_Company_OWLClass = owlModel.createOWLNamedSubclass("企業", applicant_OWLClass);
+		applicant_School_OWLClass = owlModel.createOWLNamedSubclass("學校", applicant_OWLClass);
 	}
 	
 	/**
@@ -104,20 +148,19 @@ public class OntologyConstruction {
 	 * - 專利名稱
 	 * - 申請日
 	 * - 參考文獻
-	 * - 發明人
-	 * - 申請人
 	 */
 	private void CreateOWLDataProperty() throws Exception {
 		patentName_OWLDataProperty = owlModel.createOWLDatatypeProperty("專利名稱");
 		applicationDate_OWLDataProperty = owlModel.createOWLDatatypeProperty("申請日");
 		reference_OWLDataProperty = owlModel.createOWLDatatypeProperty("參考文獻");
-		inventor_OWLDataProperty = owlModel.createOWLDatatypeProperty("發明人");
-		applicant_OWLDataProperty = owlModel.createOWLDatatypeProperty("申請人");
+		//inventor_OWLDataProperty = owlModel.createOWLDatatypeProperty("發明人");
+		//applicant_OWLDataProperty = owlModel.createOWLDatatypeProperty("申請人");
 	}
 	
 	/**
 	 * ObjectProperty:
 	 * - is_referenced_by(被參考)："專利編號" is_referenced_by "專利編號"
+	 * - 專利類型："專利編號" 專利類型為 "專利類型"
 	 */
 	private void CreateOWLObjectProperty() throws Exception {
 		// 建立 "is_referenced_by(被參考)" 物件屬性，Domain 和 Range 都設為 "專利編號"
@@ -125,16 +168,33 @@ public class OntologyConstruction {
 		isReferencedBy_OWLObjectProperty = owlModel.createOWLObjectProperty("is_referenced_by(被參考)");
 		isReferencedBy_OWLObjectProperty.setDomain(patentID_OWLClass);
 		isReferencedBy_OWLObjectProperty.setRange(patentID_OWLClass);
+		// 建立 "專利類型為" 物件屬性，Domain 設為 "專利編號"，Range 設為 "專利類型"
+		// "專利編號" 專利類型為 "專利類型"
+		patentType_OWLObjectProperty = owlModel.createOWLObjectProperty("專利類型為");
+		patentType_OWLObjectProperty.setDomain(patentID_OWLClass);
+		patentType_OWLObjectProperty.setRange(patentType_OWLClass);
 	}
 	
 	/**
-	 * 在 "專利編號" 的實例中設定 "資料屬性"：專利名稱、申請日、發明人、申請人
+	 * Individual
+	 * - 發明(Invention)
+	 * - 新型(Model)
+	 * - 新式樣/設計(Design)
+	 */
+	private void CreateOWLIndividual() throws Exception {
+		patentType_Invention_OWLIndividual = getOWLIndividual(patentType_OWLClass, "發明(Invention)");
+		patentType_Model_OWLIndividual = getOWLIndividual(patentType_OWLClass, "新型(Model)"); 
+		patentType_Design_OWLIndividual = getOWLIndividual(patentType_OWLClass, "新式樣/設計(Design)");
+	}
+	
+	/**
+	 * 在 "專利編號" 的實例中設定 "資料屬性"：專利名稱、申請日
 	 */
 	private void PatentID_OWLIndividual_AddDataPropertyValue() throws Exception {
 		patentID_OWLIndividual.addPropertyValue(patentName_OWLDataProperty, patentName);
 		patentID_OWLIndividual.addPropertyValue(applicationDate_OWLDataProperty, patentApplicationDate);
-		patentID_OWLIndividual.addPropertyValue(inventor_OWLDataProperty, patentInventor);
-		patentID_OWLIndividual.addPropertyValue(applicant_OWLDataProperty, patentApplicant);
+		//patentID_OWLIndividual.addPropertyValue(inventor_OWLDataProperty, patentInventor);
+		//patentID_OWLIndividual.addPropertyValue(applicant_OWLDataProperty, patentApplicant);
 	}
 	
 	/**
@@ -153,6 +213,9 @@ public class OntologyConstruction {
 		else return _OWLIndividual;
 	}
 	
+	/**
+	 * 如果 該筆專利沒有 "參考文獻"，就不會建立 "is_referenced_by(被參考)" 物件屬性 關聯
+	 */
 	private void BuildRelationships_PatentsAreReferencedByPatents() throws Exception {
 		//String[] patent_reference_ary = patent_references.split("; ");
 		// 正規表達式：找出 參考文獻中 所有的 專利編號 (任何國家、單位的專利編號)
