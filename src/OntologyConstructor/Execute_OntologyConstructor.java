f1364c65079c6e852a88f980bbd53d56b017476b
titangene <titangene.tw@gmail.com>
2018-06-20 10:20:22 +0800

    Remove old code: OntologyConstruction, Execute_OntologyConstructor

--- src/OntologyConstructor/Execute_OntologyConstructor.java
+++
@@ -1,8 +0,0 @@
-package OntologyConstructor;
-
-public class Execute_OntologyConstructor {
-	public static void main(String[] args) {
-		OntologyConstruction oc = new OntologyConstruction();
-		oc.CreateOntology();
-	}
-}
